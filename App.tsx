df423caba43d51ee9bef06409e87ca4f4691765b
Laia <clementecrespo.laia@gmail.com>
2023-11-06 20:17:45 +0100

    main

--- App.tsx
+++ App.tsx
@@ -14,30 +14,16 @@ const App = () => {
   return (
     <View>
       <StatusBar/>
-      <Text style={styles.sectionTitle}>Welcome to hell</Text>
+      <Text style={styles.sectionTitle}>Welcome to helll</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
     textAlign:'center',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-    color: 'purple',
-    fontSize: 18,
   },
 });
 
